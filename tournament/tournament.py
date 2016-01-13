07a87baba673d7f412055e3b7da65cd11b9df5de
CJ <vssrcj@gmail.com>
2016-01-13 20:34:28 +0200

    Add documentation to python and sql

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-# 
-# tournament.py -- implementation of a Swiss-system tournament
-#
+"""tournament.py -- implementation of a Swiss-system tournament"""
 
 import psycopg2
-import random
+#import random
+
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -13,46 +12,46 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    DB = connect()
-    c = DB.cursor()
-    c.execute("delete from results;")
-    DB.commit()
-    DB.close()
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("delete from results;")
+    connection.commit()
+    connection.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    DB = connect()
-    c = DB.cursor()
-    c.execute("delete from people;")
-    DB.commit()
-    DB.close()
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("delete from people;")
+    connection.commit()
+    connection.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    DB = connect()
-    c = DB.cursor()
-    c.execute("select count(*) from people;")
-    row = c.fetchone()
-    DB.close()
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("select count(*) from people;")
+    row = cursor.fetchone()
+    connection.close()
     return row[0]
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
-    """ 
-    DB = connect()
-    c = DB.cursor()
-    c.execute("insert into people(name) values(%s);", (name,))
-    DB.commit()
-    DB.close()
+    """
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("insert into people(name) values(%s);", (name,))
+    connection.commit()
+    connection.close()
 
 
 def playerStandings():
@@ -68,51 +67,60 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    DB = connect()
-    c = DB.cursor()
-    c.execute("select id, name, wins, matches from playerStandings;")
-    rows = c.fetchall()
-    DB.close()
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("select id, name, wins, matches from playerStandings;")
+    rows = cursor.fetchall()
+    connection.close()
     return rows
 
 
-def reportMatch(winner, loser, draw = False):
+def reportMatch(winner, loser, draw=False):
     """Records the outcome of a single match between two players.
 
     Args:
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    DB = connect()
-    c = DB.cursor()
-    c.execute("insert into results(winner_id, loser_id, draw) values(%s,%s,%s);",
-        (winner,loser,draw))
-    DB.commit()
-    DB.close()
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("insert into results(winner_id, loser_id, draw) "
+        "values(%s,%s,%s);", (winner, loser, draw))
+    connection.commit()
+    connection.close()
 
 
 def byePlayer(paired_players):
+    """Gets the player that hasn't been paired up yet.
 
-    if not paired_players: return None
-    DB = connect()
-    c = DB.cursor()
-    c.execute("select id, name from people where id not in %s;",
+    Args:
+        paired_players: a list that contains all the players that have been
+                        paired up
+
+    Returns:
+        A tuple of in the form of (id, name)
+    """
+    if not paired_players:
+        return None
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("select id, name from people where id not in %s;",
         (tuple(paired_players),))
-    bye_player = c.fetchone()
+    bye_player = cursor.fetchone()
 
-    DB.close()
+    connection.close()
 
     return bye_player
 
- 
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -122,55 +130,50 @@ def swissPairings():
     """
     player_count = countPlayers()
 
-    DB = connect()
-    c = DB.cursor()
-    c.execute("select id1, name1, id2, name2 from matchupsRemaining;")
-    pairings = []
-    paired_players = []
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("select id1, name1, id2, name2 from matchupsRemaining;")
 
-    for row in c:
-        
-        if row[0] not in paired_players and row[2] not in paired_players and row[0] != row[2]:
-            
+    pairings = [] #the return list of tuples
+    paired_players = [] #the ids of paired players
+
+    for row in cursor:
+        if row[0] not in paired_players and row[2] not in paired_players:
             paired_players.extend([row[0], row[2]])
             pairings.append(row)
-            if len(paired_players) >= player_count - 1: break
+            if len(paired_players) >= player_count - 1:
+                break
 
-    DB.close()
+    connection.close()
 
     if player_count % 2 != 0:
         bye_player = byePlayer(paired_players)
-        if bye_player is None: return []
-        else: pairings.extend([bye_player,bye_player])
+        if bye_player is None:
+            return []
+        else: pairings.extend([bye_player, bye_player])
 
     return pairings
 
 
-def test():
-
-    for i in range(20):
-        k = random.getrandbits(1)
-        print k
-        t = True if k == 1 else False
-        print t
-
-
-def sampleTournament():
-
-    pairings = swissPairings()
-    round = 1
-
-    while pairings != []:
-        print 'Round ' + str(round)
-        print 'Pairings ' + str(pairings)
-        for pair in pairings:
-            if pair[0] == pair[1]: reportMatch(pair[0],pair[0])
-            else:
-                first_win = True if random.getrandbits(1) == 1 else False
-                draw = True if random.getrandbits(1) == 1 else False
-                if first_win: reportMatch(pair[0],pair[2],draw)
-                else: reportMatch(pair[2],pair[0],draw)
-        print 'Standings ' + str(playerStandings())    
-        pairings = swissPairings()
-        round = round + 1
+#def sampleTournament():
+#    """This is a test to run a full tournament with data already in the db"""
+#    pairings = swissPairings()
+#    current_round = 1
+#
+#    while pairings != []:
+#        print 'Round ' + str(current_round)
+#        print 'Pairings ' + str(pairings)
+#        for pair in pairings:
+#            if pair[0] == pair[1]:
+#                reportMatch(pair[0], pair[0])
+#            else:
+#                first_win = True if random.getrandbits(1) == 1 else False
+#                draw = True if random.getrandbits(1) == 1 else False
+#                if first_win:
+#                    reportMatch(pair[0], pair[2], draw)
+#                else:
+#                    reportMatch(pair[2], pair[0], draw)
+#        print 'Standings ' + str(playerStandings())
+#        pairings = swissPairings()
+#        current_round = current_round + 1
 
--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -1,10 +1,4 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
 DROP TABLE IF EXISTS results CASCADE;
 DROP TABLE IF EXISTS people CASCADE;
@@ -12,11 +6,18 @@ DROP TABLE IF EXISTS people CASCADE;
 CREATE TABLE people
 ( 
     id serial primary key,
-    name text not null
+    name text not nulls
 );
 
 CREATE TABLE results
 (   
+-- If a draw occurs, then it doesn't matter if which person is stored under
+-- winner_id or loser_id
+--
+-- If there an uneven amount of players, then one person receives a bye.
+-- This means that winner_id and loser_id is both the id of the person who
+-- receives a bye and the draw must be false.
+--
     winner_id int references people(id),
     loser_id int references people(id),
     draw boolean not null,
@@ -24,92 +25,32 @@ CREATE TABLE results
     primary key (winner_id, loser_id)
 );
 
---CREATE TABLE tournament
---(
---	id serial primary key,
---	name text not null
---);
+-- The following is how a multiple tournament setup would've worked
+--
+-- CREATE TABLE tournament
+-- (
+--	 id serial primary key,
+--	 name text not null
+-- );
+--
+-- Because the relationship between people and tournament is many-to-many the
+-- following mapping table would be used
+--
+-- CREATE TABLE tournamentPlayers
+-- (
+--	 tournament_id int references tournament(id),
+--	 people_id int references people(id),
+--	 primary key(tournament_id, people_id)
+-- );
+--
+-- Add tournament.id to the table results as another primary key
 
---Because the relationship between people and tournament is many-to-many the
---following mapping table would be used
-
---CREATE TABLE tournamentPlayers
---(
---	tournament_id int references tournament(id),
---	people_id int references people(id),
---	primary key(tournament_id, people_id)
---);
-
--- Add tournament.id to results as another primary key
-
-
-INSERT INTO people (name) VALUES('CJ');
-INSERT INTO people (name) VALUES('Pa');
-INSERT INTO people (name) VALUES('Ma');
-INSERT INTO people (name) VALUES('Wes');
-INSERT INTO people (name) VALUES('CJ2');
-INSERT INTO people (name) VALUES('Pa2');
-INSERT INTO people (name) VALUES('Ma2');
-INSERT INTO people (name) VALUES('Wes2');
-INSERT INTO people (name) VALUES('CJ3');
-INSERT INTO people (name) VALUES('Pa3');
-INSERT INTO people (name) VALUES('Ma3');
-INSERT INTO people (name) VALUES('Wes3');
-INSERT INTO people (name) VALUES('CJ4');
-INSERT INTO people (name) VALUES('Pa4');
-INSERT INTO people (name) VALUES('Ma4');
-INSERT INTO people (name) VALUES('Wes4');
---INSERT INTO people (name) VALUES('Ann');
---INSERT INTO people (name) VALUES('Ben');
---INSERT INTO people (name) VALUES('Dan');
---INSERT INTO people (name) VALUES('Elkl');
-
---INSERT INTO results VALUES(1,2); 
---INSERT INTO results VALUES(3,4);
---INSERT INTO results VALUES(5,5);
-
---1 1,2 0,3 1,4 0,5*1
-
---INSERT INTO results VALUES(1,1); 
---INSERT INTO results VALUES(2,4);
---INSERT INTO results VALUES(3,5);
-
---1*2,2 1,3 2,4 0,5*1
-
---INSERT INTO results VALUES(2,2); 
---INSERT INTO results VALUES(1,3);
---INSERT INTO results VALUES(4,5);
-
---1*3,2*2,3 2,4 1,5*1
-
---INSERT INTO results VALUES(1,5);
---INSERT INTO results VALUES(2,4);
---INSERT INTO results VALUES(3,3);*
-
-
-
---INSERT INTO results VALUES(1,3);
---INSERT INTO results VALUES(2,5);
---INSERT INTO results VALUES(4,4);*
---INSERT INTO results VALUES(4,5);
---INSERT INTO results VALUES(2,3);
---INSERT INTO results VALUES(1,1);*
-
-
---INSERT INTO results VALUES(5,6);
---INSERT INTO results VALUES(7,8);
---INSERT INTO results VALUES(1,3);
---INSERT INTO results VALUES(5,7);
---INSERT INTO results VALUES(2,4);
---INSERT INTO results VALUES(6,8);
 
 CREATE VIEW matchesWon AS
--- Returns the id and the amount of matches won
--- (will only return if at least one match is won)
 
     SELECT 
-        winner_id id, 
-        count(*) count
+        winner_id id,   -- player's id
+        count(*) count  -- amount of matches won (will not return anything if 0)
     FROM results
     WHERE draw = false
     GROUP BY winner_id;
@@ -122,26 +63,27 @@ CREATE VIEW matchesPlayed AS
 	name, 
 	(SELECT count(*)
  	 FROM results
- 	 WHERE results.winner_id != results.loser_id 
-	 AND (results.winner_id = people.id
- 	 OR results.loser_id = people.id)) count
+ 	 WHERE results.winner_id != results.loser_id    -- discount byes
+	 AND (results.winner_id = people.id             -- match as winner
+ 	 OR results.loser_id = people.id)) count        -- match as loser
     FROM people
 	ORDER BY count DESC;
 
 
-CREATE VIEW omw AS
+CREATE VIEW omw AS -- opponent match wins
 
     SELECT 
-        people.id, 
+        people.id,              -- player's id
         sum((SELECT count 
              FROM matchesWon 
              WHERE matchesWon.id = 
-                CASE WHEN people.id = winner_id THEN loser_id
-                ELSE winner_id 
+                CASE WHEN people.id = winner_id THEN 
+                    loser_id    -- if player was the winner, count loser's wins
+                ELSE winner_id  -- if player was the loser, count winner's wins
                 END)) count
     FROM people LEFT JOIN results
     ON people.id = winner_id OR people.id = loser_id 
-    WHERE winner_id != loser_id
+    WHERE winner_id != loser_id -- discount byes
     GROUP BY people.id;
 
 
@@ -150,8 +92,8 @@ CREATE VIEW playerStandings AS
     SELECT 
         matchesPlayed.id, 
         matchesPlayed.name, 
-        coalesce(matchesWon.count,0) wins, 
-        coalesce(omw.count,0) omw,
+        coalesce(matchesWon.count,0) wins,  -- 0 if null
+        coalesce(omw.count,0) omw,          -- 0 if null
         matchesPlayed.count matches
     FROM matchesPlayed
     LEFT JOIN matchesWon 
@@ -162,6 +104,7 @@ CREATE VIEW playerStandings AS
 
 CREATE VIEW matchups AS
 -- This returns all the possible matchups/pairings that can occur
+-- (excluding byes)
 
 	SELECT a.id id1, a.name name1, b.id id2, b.name name2, 
         abs(a.wins - b.wins) as winDifference,
@@ -173,23 +116,16 @@ CREATE VIEW matchups AS
 CREATE VIEW matchupsRemaining AS
 -- Returns matchups/pairings that haven't occured yet.  It does so by returning
 -- the matchups that exists in the matchups VIEW, but does not occur
--- in the results TABLE.  The LEFT JOIN acts as a EXCEPT statement.,
+-- in the results TABLE.  The LEFT JOIN acts as an EXCEPT statement.,
 
 	SELECT id1, name1, id2, name2, winDifference, matchesCombined
 	FROM matchups
 	LEFT JOIN results a 
 	ON matchups.id1 = a.winner_id
 	AND matchups.id2 = a.loser_id
-    LEFT JOIN results b    
+    LEFT JOIN results b 
 	ON matchups.id1 = b.loser_id
 	AND matchups.id2 = b.winner_id
 	WHERE a.winner_id IS NULL
     AND b.loser_id IS NULL
     ORDER BY winDifference, matchesCombined, id1, id2;
-
-
-
-
-
-
-	
